d7e8f685bb15a8faced9b62398d34760dc54de26
ChangDaze <explision000@gmail.com>
2025-02-06 03:57:59 +0800

    [feature] 0258AddDigits

---
+++ ProblemSolveConsole/LeetCode/0258AddDigits.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0258AddDigits
+    {
+        public int AddDigits(int num)
+        {
+
+            #region 暴力解
+            int current = num;
+            while (current >= 10)
+            {
+                int temp = current;
+                int tempCurrent = 0;
+                while (temp > 0)
+                {
+                    tempCurrent += temp % 10;
+                    temp /= 10;
+                }
+                current = tempCurrent;
+            }
+            return current;
+            #endregion
+            #region 數根
+            //if (num == 0) return 0;
+            //else if (num % 9 == 0) return 9;
+            //else return num % 9;
+            #endregion
+
+
+        }
+
+        //用數學特性解可以超快，這裡就是可以用數根特性
+        //數根說明
+        //https://zh.wikipedia.org/zh-tw/%E6%95%B8%E6%A0%B9
+        //數根說明2
+        //https://ithelp.ithome.com.tw/m/articles/10238613
+        //數根解法
+        //https://leetcode.com/problems/add-digits/solutions/1754040/c-recursion-and-iteration-and-o-1-approaches-fast-solutions/
+
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0258AddDigitsTest.cs
@@ -0,0 +1,37 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0258AddDigitsTest
+    {
+        private _0258AddDigits _0258AddDigits;
+
+        public _0258AddDigitsTest()
+        {
+            _0258AddDigits = new _0258AddDigits();
+        }
+        [Fact]
+        public void Test1()
+        {
+            var result = _0258AddDigits.AddDigits(38);
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public void Test2()
+        {
+            var result = _0258AddDigits.AddDigits(0);
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void Test3()
+        {
+            var result = _0258AddDigits.AddDigits(10);
+            Assert.Equal(1, result);
+        }
+    }
+}
