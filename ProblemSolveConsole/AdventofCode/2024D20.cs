28bf28273aba4e1501cf2d8121d08893c1afce03
ChangDaze <explision000@gmail.com>
2025-05-25 14:24:30 +0800

    [main][feature]Aoc 2024 Day20 Q2

--- ProblemSolveConsole/AdventofCode/2024D20.cs
+++ ProblemSolveConsole/AdventofCode/2024D20.cs
@@ -16,6 +16,7 @@ namespace ProblemSolveConsole.AdventofCode
             {
                 string input = sr.ReadToEnd();
                 Console.WriteLine(CountValidCheats(input, 100));
+                Console.WriteLine(CountValidCheats(input, 100, 20));
             }
         }
         public int CountValidCheats(string input, int standard)
@@ -141,15 +142,89 @@ namespace ProblemSolveConsole.AdventofCode
             return record;            
         }
         /// <summary>
-        /// 因為只有一條路所以也不用了
+        /// 擴充cheatSteps版本
         /// </summary>
-        private bool CheckRoad(char[][] map, (int x, int y) position)
+        private Dictionary<int, List<((int x, int y) sPoint, (int x, int y) ePoint)>> CollectList((string[][] map, (int x, int y) start, (int x, int y) end) data, List<(int x, int y)> path, int cheatSteps)
         {
-            if(map[position.x][position.y] != '.')
+            //前提 cheat 只能一次，
+            //(原本以為不行卡了超久...，幸好有用測試題目對過各組數量，不然會直接撞壁)且中間可以經過一般道路(https://www.reddit.com/r/adventofcode/comments/1hivatr/comment/m31tsgs/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
+            Dictionary<int, List<((int x, int y) sPoint, (int x, int y) ePoint)>> record = new();
+            int[][] directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]; //上下左右
+            int max = data.map.Length;
+            foreach (var basePoint in path)
             {
-                return false;
+                //BFS讓進出都只會用最短路徑看一次
+                int sStep = Convert.ToInt32(data.map[basePoint.x][basePoint.y]);
+                HashSet<(int x, int y)> passed = new();
+                Queue<(int x, int y, int step)> queue = new Queue<(int x, int y, int step)>();
+                queue.Enqueue((basePoint.x, basePoint.y, 0));
+
+                while (queue.Count > 0)
+                {
+                    (int x, int y, int step) current = queue.Dequeue();
+                    if(
+                        current.step > cheatSteps || //檢查步數
+                        current.x < 0 || current.y <0 || current.x >= data.map.Length || current.y >= data.map.Length ||//檢查範圍
+                        passed.Contains((current.x, current.y))//確認還沒來過
+                    )
+                    {
+                        continue;
+                    }
+
+                    //紀錄已來過讓BFS生效
+                    //【注意】包含ePoint => 因為相同起點終點就只留最短路徑了
+                    passed.Add((current.x, current.y));
+
+                    //結算
+                    if (
+                        int.TryParse(data.map[current.x][current.y], out int eStep) &&
+                        eStep > (sStep + current.step) //有節省才結算
+                    ) 
+                    {
+                        int diff = eStep - sStep - current.step;
+                        var sp = ((basePoint.x, basePoint.y), (current.x, current.y));
+                        if (record.TryGetValue(diff, out var list))
+                        {
+                            list.Add(sp);
+                        }
+                        else
+                        {
+                            record[diff] = new() { sp };
+                        }
+                    }
+                    //中間可以經過一般道路，所以是數字且沒要結算也能繼續後面的路
+
+                    //繼續走
+                    int newStep = current.step + 1;
+                    foreach (int[] direction in directions)
+                    {
+                        int newX = current.x + direction[0];
+                        int newY = current.y + direction[1];
+                        queue.Enqueue((newX, newY, newStep));
+                    }
+                }
             }
-            return true;
+
+            return record;
+        }
+        public int CountValidCheats(string input, int standard, int cheatSteps)
+        {
+
+            var data = GetData(input);
+            List<(int x, int y)> path = MarkMap(data);
+            var record = CollectList(data, path, cheatSteps);
+
+            int count = 0;
+
+            foreach (var item in record)
+            {
+                if (item.Key >= standard)
+                {
+                    count += item.Value.Count;
+                }
+            }
+
+            return count;
         }
     }
 }
