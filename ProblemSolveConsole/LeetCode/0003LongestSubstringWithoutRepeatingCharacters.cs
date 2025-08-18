371c51f8320104c646a43e85e247452669aff573
ChangDaze <explision000@gmail.com>
2025-08-19 05:55:14 +0800

    [main][feature]leetcode 0003

---
+++ ProblemSolveConsole/LeetCode/0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0003LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            //有出現第二次的字就能中斷了
+            //需要連續
+            //可以用hash和two pointer來遞增遞減
+            //也不用紀錄原字串，所以算長度即可
+
+            //https://leetcode.com/problems/longest-substring-without-repeating-characters/solutions/3649636/3-method-s-c-java-python-beginner-friendly/
+            //第一種方法一樣
+            //第二種用記錄index方式優化讓window縮減時不用一一檢查，也不用重複新增移除set
+            //第三種方式是用储存方式優化第二種方法
+
+            HashSet<char> result = new HashSet<char>();
+            int leftPointer = 0;
+            int strike = 0;
+            int maxStrike = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (result.Contains(s[i]))
+                {
+                    if (strike > maxStrike)
+                    {
+                        maxStrike = strike;
+                    }
+
+                    do
+                    {
+                        result.Remove(s[leftPointer]);
+                        strike--;
+                        leftPointer++;                        
+                    }
+                    while (result.Contains(s[i]));
+                }
+
+                result.Add(s[i]);
+                strike++;
+            }
+
+            if (strike > maxStrike)
+            {
+                maxStrike = strike;
+            }
+
+            return maxStrike;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0003LongestSubstringWithoutRepeatingCharactersTest.cs
@@ -0,0 +1,51 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0003LongestSubstringWithoutRepeatingCharactersTest
+    {
+        private _0003LongestSubstringWithoutRepeatingCharacters _0003LongestSubstringWithoutRepeatingCharacters;
+
+        public _0003LongestSubstringWithoutRepeatingCharactersTest()
+        {
+            _0003LongestSubstringWithoutRepeatingCharacters = new _0003LongestSubstringWithoutRepeatingCharacters();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+            var result = _0003LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("abcabcbb");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+            var result = _0003LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("bbbbb");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+            var result = _0003LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("pwwkew");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+
+            var result = _0003LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("ababab");
+            Assert.Equal(2, result);
+        }
+    }
+}
