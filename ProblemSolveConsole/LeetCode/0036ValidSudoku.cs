e3f2d4b3d0300b38e7604e6f48256edb5e3bf487
ChangDaze <explision000@gmail.com>
2025-11-04 22:51:55 +0800

    [main][feat]36. Valid Sudoku

---
+++ ProblemSolveConsole/LeetCode/0036ValidSudoku.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0036ValidSudoku
+    {
+#if false //brute force => 靠時間
+        public bool IsValidSudoku(char[][] board)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                HashSet<int> row = new HashSet<int>();
+                HashSet<int> column = new HashSet<int>();
+                for (int j = 0; j < 9; j++)
+                {
+                    //解row
+                    if (board[i][j] != '.')
+                    {
+                        if (row.Contains(board[i][j]))
+                        {
+                            return false;
+                        }
+                        row.Add(board[i][j]);
+                    }
+
+                    //解column
+                    if (board[j][i] != '.')
+                    {
+                        if (column.Contains(board[j][i]))
+                        {
+                            return false;
+                        }
+                        column.Add(board[j][i]);
+                    }
+                }
+
+            }
+
+            
+
+            //解block
+            for (int k = 0; k < 9; k = k + 3)
+            {
+                for (int l = 0; l < 9; l = l + 3)
+                {
+                    HashSet<int> block = new HashSet<int>();
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (board[i+k][j+l] != '.')
+                            {
+                                if (block.Contains(board[i + k][j + l]))
+                                {
+                                    return false;
+                                }
+                                block.Add(board[i + k][j + l]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+#endif
+        public bool IsValidSudoku(char[][] board)
+        {
+            //總共18個區域需要比較一次建起來比brute force => 靠空間
+            List<HashSet<int>> rows = new List<HashSet<int>>();
+            List<HashSet<int>> columns = new List<HashSet<int>>();
+            List<HashSet<int>> blocks = new List<HashSet<int>>();
+
+            //建立容器
+            for (int i = 0; i < 9; i++)
+            {
+                rows.Add(new HashSet<int>());
+                columns.Add(new HashSet<int>());
+                blocks.Add(new HashSet<int>());
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] == '.')
+                    {
+                        continue;
+                    }
+
+                    if (rows[i].Contains(board[i][j])){
+                        return false;
+                    }
+                    rows[i].Add(board[i][j]);
+
+                    if (columns[j].Contains(board[i][j]))
+                    {
+                        return false;
+                    }
+                    columns[j].Add(board[i][j]);
+
+                    int blockIndex = (i / 3) * 3 + (j / 3);
+                    /*
+                     ex:3層，每層3塊共9塊
+                     (row/3) 決定它在第幾層（0,1,2）
+                     每層有 3 塊，所以換層把前面幾層(row)pass的區塊(*3)加上，才加層這層的第幾區塊(col) → * 3
+                     (col/3) 決定是該層的第幾塊（0,1,2）
+                     */
+                    if (blocks[blockIndex].Contains(board[i][j]))
+                    {
+                        return false;
+                    }
+                    blocks[blockIndex].Add(board[i][j]);
+                }
+            }
+
+            return true;
+        }
+
+        //https://leetcode.com/problems/valid-sudoku/solutions/15472/shortsimple-java-using-strings-by-stefan-rmwb/
+        //結果自己encode每種有效情境+hashset最清晰...XD
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0036ValidSudokuTest.cs
@@ -0,0 +1,61 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0036ValidSudokuTest
+    {
+        private _0036ValidSudoku _0036ValidSudoku;
+
+        public _0036ValidSudokuTest()
+        {
+            _0036ValidSudoku = new _0036ValidSudoku();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            bool result = _0036ValidSudoku.IsValidSudoku(
+                new char[][]
+{
+    new char[] {'5','3','.','.','7','.','.','.','.'},
+    new char[] {'6','.','.','1','9','5','.','.','.'},
+    new char[] {'.','9','8','.','.','.','.','6','.'},
+    new char[] {'8','.','.','.','6','.','.','.','3'},
+    new char[] {'4','.','.','8','.','3','.','.','1'},
+    new char[] {'7','.','.','.','2','.','.','.','6'},
+    new char[] {'.','6','.','.','.','.','2','8','.'},
+    new char[] {'.','.','.','4','1','9','.','.','5'},
+    new char[] {'.','.','.','.','8','.','.','7','9'}
+}
+            );
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            bool result = _0036ValidSudoku.IsValidSudoku(
+                new char[][]
+{
+    new char[] {'8','3','.','.','7','.','.','.','.'},
+    new char[] {'6','.','.','1','9','5','.','.','.'},
+    new char[] {'.','9','8','.','.','.','.','6','.'},
+    new char[] {'8','.','.','.','6','.','.','.','3'},
+    new char[] {'4','.','.','8','.','3','.','.','1'},
+    new char[] {'7','.','.','.','2','.','.','.','6'},
+    new char[] {'.','6','.','.','.','.','2','8','.'},
+    new char[] {'.','.','.','4','1','9','.','.','5'},
+    new char[] {'.','.','.','.','8','.','.','7','9'}
+}
+            );
+
+            Assert.False(result);
+        }
+    }
+}
