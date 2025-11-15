7c0b787805dafb2da70f5442f654e3916d371496
ChangDaze <explision000@gmail.com>
2025-11-15 21:07:44 +0800

    [main][feat]0049 GroupAnagrams

---
+++ ProblemSolveConsole/LeetCode/0049 GroupAnagrams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0049_GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            //最簡單的map + sort暴力解而已
+            //time O(n · k log k) => k log k是OrderBy
+            //space O(n · k)
+            Dictionary<string, List<string>> maps = new Dictionary<string, List<string>>();
+
+            foreach (string str in strs)
+            {
+                string key = string.Join("", str.ToList().OrderBy(x => x));
+                if (maps.ContainsKey(key))
+                {
+                    maps[key].Add(str);
+                }
+                else
+                {
+                    maps[key] = new List<string>() { str};
+                }
+            }
+
+            return maps.Values.Select(x => (IList<string>)new List<string>(x)).ToList(); //(IList<string>)沒做upcast(向上轉型，零成本，且參考一致，更多是單純告訴編譯器這裡是用甚麼而已)會吃不下去
+        }
+
+        //https://leetcode.com/problems/group-anagrams/solutions/19176/share-my-short-java-solution-by-legendar-0yrd/
+        //這個人方法一樣，不過他的方法二是自己用26字母array令建key(這裡單純用int[]應該也可)，就能不sort，會把O(k log k)濃縮成O(k)，理論上在字串越長就越有效率 => 這種應該要能馬上想到@@
+
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0049 GroupAnagramsTest.cs
@@ -0,0 +1,48 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0049_GroupAnagramsTest
+    {
+        private _0049_GroupAnagrams _0049_GroupAnagrams;
+
+        public _0049_GroupAnagramsTest()
+        {
+            _0049_GroupAnagrams = new _0049_GroupAnagrams();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            IList<IList<string>>  result = _0049_GroupAnagrams.GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
+            IList<IList<string>> expect = [["bat"],["nat","tan"],["ate","eat","tea"]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            IList<IList<string>> result = _0049_GroupAnagrams.GroupAnagrams([""]);
+            IList<IList<string>> expect = [[""]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            IList<IList<string>> result = _0049_GroupAnagrams.GroupAnagrams(["a"]);
+            IList<IList<string>> expect = [["a"]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        private string Normalize(IList<IList<string>> list) =>
+        string.Join(";", list
+            .Select(inner => string.Join(",", inner.OrderBy(x => x)))
+            .OrderBy(s => s));
+    }
+}
