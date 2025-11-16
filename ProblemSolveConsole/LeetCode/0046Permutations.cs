38d64e028d968b2f4c52672f69bdf1a2a2143ec6
ChangDaze <explision000@gmail.com>
2025-11-16 13:32:58 +0800

    [main][feat]0046Permutations

---
+++ ProblemSolveConsole/LeetCode/0046Permutations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0046Permutations
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            //time : O(n·n!)
+            //space: O(n·n!)
+
+
+            IList<IList<int>> result = new List<IList<int>>();
+            List<int> permutation = new List<int>();
+            bool[] used = new bool[nums.Length];
+            BackTracking(nums, permutation, used, result);
+            return result;
+        }
+
+        private void BackTracking(int[] nums, List<int> permutation, bool[] used, IList<IList<int>> result)
+        {
+            if(permutation.Count == nums.Length)
+            {
+                result.Add(new List<int>(permutation));
+                return;
+            }
+
+            for(int i = 0; i< nums.Length; i++)
+            {
+                if (used[i]) { continue; }
+                permutation.Add(nums[i]);
+                used[i] = true;
+                BackTracking(nums, permutation, used, result);
+                permutation.RemoveAt(permutation.Count - 1);
+                used[i] = false;
+            }
+        }
+
+        //https://leetcode.com/problems/permutations/solutions/18247/my-elegant-recursive-c-solution-with-inl-yci2/?page=3
+        //這個解法很有趣，是inplace swap，簡單講就是實質意義上的permutation都做一次，但建立在array內容是distinct的，有點難想像如何證明，但這是事實
+
+        //每一層遞迴都會把一個「位置」固定（num[0] 固定 → num[1] 固定 → …）
+        //begin代表每層，且只會和後面的SWAP i >= begin
+        //然後每進下層拆成更小的問題，有點dp和divide and conquer的感覺
+        //理論上效率更好，可以當作一個額外知識參考
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0046PermutationsTest.cs
@@ -0,0 +1,48 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0046PermutationsTest
+    {
+        private _0046Permutations _0046Permutations;
+
+        public _0046PermutationsTest()
+        {
+            _0046Permutations = new _0046Permutations();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            IList<IList<int>> result = _0046Permutations.Permute([1, 2, 3]);
+            IList<IList<int>> expect = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            IList<IList<int>> result = _0046Permutations.Permute([0, 1]);
+            IList<IList<int>> expect = [[0, 1], [1, 0]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            IList<IList<int>> result = _0046Permutations.Permute([1]);
+            IList<IList<int>> expect = [[1]];
+            Assert.Equal(Normalize(expect), Normalize(result));
+        }
+
+        private string Normalize(IList<IList<int>> list) =>
+string.Join(";", list
+    .Select(inner => string.Join(",", inner.OrderBy(x => x)))
+    .OrderBy(s => s));
+    }
+}
