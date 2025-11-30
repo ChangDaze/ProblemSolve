d0c5ce6c56d40830b01481773b871b35079967af
ChangDaze <explision000@gmail.com>
2025-12-01 02:28:19 +0800

    [main][feat]0071SimplifyPath

---
+++ ProblemSolveConsole/LeetCode/0071SimplifyPath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0071SimplifyPath
+    {
+        public string SimplifyPath(string path)
+        {
+            // time：O(n)
+            // space：O(n)
+
+            string[] paths = path.Split('/');
+            List<string> stack = new List<string>();
+
+            foreach (string p in paths)
+            {
+                if (p == "" || p == ".")// // || .
+                {
+                    continue;
+                }
+                else if( p == "..")// ..
+                {
+                    if(stack.Count > 0)
+                    {
+                        stack.RemoveAt(stack.Count - 1);
+                    }
+                }
+                else
+                {
+                    stack.Add(p);
+                }
+            }
+
+            return "/" + String.Join("/", stack);
+        }
+
+        //https://leetcode.com/problems/simplify-path/solutions/1847357/c-easy-stack-simple-explained-algorithm-qhwp2/
+        //這位就是逐字處理，沒有像我靠工具簡化，方法一樣
+        //https://leetcode.com/problems/simplify-path/solutions/1847526/best-explanation-ever-possible-not-a-cli-jz2y/
+        //這位方法也一樣
+        //不過他們都用stack，我因為不想倒過來就簡單用list而已
+
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0071SimplifyPathTest.cs
@@ -0,0 +1,55 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0071SimplifyPathTest
+    {
+
+        private _0071SimplifyPath _0071SimplifyPath;
+
+        public _0071SimplifyPathTest()
+        {
+            _0071SimplifyPath = new _0071SimplifyPath();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            string result = _0071SimplifyPath.SimplifyPath("/home/");
+            Assert.Equal("/home", result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string result = _0071SimplifyPath.SimplifyPath("/home//foo/");
+            Assert.Equal("/home/foo", result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string result = _0071SimplifyPath.SimplifyPath("/home/user/Documents/../Pictures");
+            Assert.Equal("/home/user/Pictures", result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            string result = _0071SimplifyPath.SimplifyPath("/../");
+            Assert.Equal("/", result);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            string result = _0071SimplifyPath.SimplifyPath("/.../a/../b/c/../d/./");
+            Assert.Equal("/.../b/d", result);
+        }
+    }
+}
