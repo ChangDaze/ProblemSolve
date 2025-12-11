d03f22f5e918c8984b82773634a14fdf10481b51
ChangDaze <explision000@gmail.com>
2025-12-12 06:31:39 +0800

    [main][feat]0074Searcha2DMatrix

---
+++ ProblemSolveConsole/LeetCode/0074Searcha2DMatrix.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0074Searcha2DMatrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            //Time complexity:O(log(m * n))
+            //Space complexity:O(1)
+            int left = 0;
+            int right = matrix.Length -1;
+            int targetRow = -1;            
+
+            //先找到該行
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (matrix[mid][0] <= target && target <= matrix[mid][matrix[mid].Length - 1]) //找到對的行才有這個效果
+                {
+                    targetRow = mid;
+                    break;
+                }
+                else if(matrix[mid][0] > target) //後面簡單判斷即可
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if(targetRow == -1)
+            {
+                return false;
+            }
+
+            //該行找到該值
+            left = 0;
+            right = matrix[targetRow].Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (matrix[targetRow][mid] > target) 
+                {
+                    right = mid - 1;
+                }
+                else if (matrix[targetRow][mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //這題要注意的可能是第一個while的binary search 變形用法，要記住怎麼使用
+        //https://leetcode.com/problems/search-a-2d-matrix/solutions/26220/dont-treat-it-as-a-2d-matrix-just-treat-u8vgk/
+        //...，這個人的做法更易懂XD，直接搭配%拆成一維，變成一般的binary search
+        //https://leetcode.com/problems/search-a-2d-matrix/solutions/1895837/c-binary-search-tree-917-explained-with-3mijz/
+        //這位很特別，他用1 row row找，所以不是O(log(m * n))，是單純O(n)，也很有趣
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0074Searcha2DMatrixTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0074Searcha2DMatrixTest
+    {
+        private _0074Searcha2DMatrix _0074Searcha2DMatrix;
+
+        public _0074Searcha2DMatrixTest()
+        {
+            _0074Searcha2DMatrix = new _0074Searcha2DMatrix();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            bool result = _0074Searcha2DMatrix.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            bool result = _0074Searcha2DMatrix.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13);
+            Assert.False(result);
+        }
+    }
+}
