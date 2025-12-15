0d1545006045ee1c982c6b37a38fdbe5b92d660b
ChangDaze <explision000@gmail.com>
2025-12-16 06:51:39 +0800

    [main][feat]2110NumberofSmoothDescentPeriodsofaStock

---
+++ ProblemSolveConsole/LeetCode/2110NumberofSmoothDescentPeriodsofaStock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _2110NumberofSmoothDescentPeriodsofaStock
+    {
+        public long GetDescentPeriods(int[] prices)
+        {
+            long result = 0;
+
+            long streaks = 1;
+
+            for (int i = prices.Length-2; i >= 0; i--)
+            {
+                if(prices[i] - prices[i+1] == 1)
+                {
+                    streaks++;
+                }
+                else
+                {
+                    result += (streaks + 1) * streaks / 2;
+                    streaks = 1;
+                }
+            }
+
+            result += (streaks + 1) * streaks / 2;
+
+            return result;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/2110NumberofSmoothDescentPeriodsofaStockTest.cs
@@ -0,0 +1,41 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _2110NumberofSmoothDescentPeriodsofaStockTest
+    {
+
+        private _2110NumberofSmoothDescentPeriodsofaStock _2110NumberofSmoothDescentPeriodsofaStock;
+
+        public _2110NumberofSmoothDescentPeriodsofaStockTest()
+        {
+            _2110NumberofSmoothDescentPeriodsofaStock = new _2110NumberofSmoothDescentPeriodsofaStock();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            long result = _2110NumberofSmoothDescentPeriodsofaStock.GetDescentPeriods([3, 2, 1, 4]);
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            long result = _2110NumberofSmoothDescentPeriodsofaStock.GetDescentPeriods([8, 6, 7, 7]);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            long result = _2110NumberofSmoothDescentPeriodsofaStock.GetDescentPeriods([1]);
+            Assert.Equal(1, result);
+        }
+    }
+}
