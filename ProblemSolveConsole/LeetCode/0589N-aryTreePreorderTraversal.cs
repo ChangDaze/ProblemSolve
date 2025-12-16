8411f3d4f4ff28dfc8796acb577715d9b15ba1ad
ChangDaze <explision000@gmail.com>
2025-12-17 06:54:41 +0800

    [main][feat]_0589N_aryTreePreorderTraversal

---
+++ ProblemSolveConsole/LeetCode/0589N-aryTreePreorderTraversal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static ProblemSolveConsole.LeetCode._0589N_aryTreePreorderTraversal;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0589N_aryTreePreorderTraversal
+    {
+        public IList<int> Preorder(Node root)
+        {
+            List<int> result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            Recursive(root, result);
+
+            return result;
+        }
+
+        private void Recursive(Node node, List<int> result)
+        {
+            result.Add(node.val);
+
+            foreach (Node sub in node.children) //會跑DFS
+            {
+                Recursive(sub, result);
+            }
+        }
+
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0589N-aryTreePreorderTraversalTest.cs
@@ -0,0 +1,37 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Node = ProblemSolveConsole.LeetCode._0589N_aryTreePreorderTraversal.Node;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0589N_aryTreePreorderTraversalTest
+    {
+
+        private _0589N_aryTreePreorderTraversal _0589N_aryTreePreorderTraversal;
+
+        public _0589N_aryTreePreorderTraversalTest()
+        {
+            _0589N_aryTreePreorderTraversal = new _0589N_aryTreePreorderTraversal();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            IList<int> result = _0589N_aryTreePreorderTraversal.Preorder(
+                new Node(1, new List<Node>() {
+                    new Node(3, new List<Node>()
+                    {
+                        new Node(5, new List<Node>()),
+                        new Node(6, new List<Node>())
+                    }),
+                    new Node(2, new List<Node>()),
+                    new Node(4, new List<Node>())
+                }));
+            Assert.Equal([1, 3, 5, 6, 2, 4], result);
+        }
+    }
+}
