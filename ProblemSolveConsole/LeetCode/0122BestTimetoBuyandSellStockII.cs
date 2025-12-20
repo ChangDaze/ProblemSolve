414c7fdd2c55475c897d204ef4ec35dacca5f312
ChangDaze <explision000@gmail.com>
2025-12-20 23:17:32 +0800

    [main][feat]0122BestTimetoBuyandSellStockII註解

--- ProblemSolveConsole/LeetCode/0122BestTimetoBuyandSellStockII.cs
+++ ProblemSolveConsole/LeetCode/0122BestTimetoBuyandSellStockII.cs
@@ -88,7 +88,7 @@ namespace ProblemSolveConsole.LeetCode
                 //計算當下值最佳
                 int cur = Math.Max(prices[i] - smallest, 0);
 
-                //計算過往最佳結果 + 當前持有(一定是>=0，因為買賣都能選擇，0是因為可能價格相等)
+                //計算過往最佳結果 + 當前持有(一定是>=0，因為買賣能同天，0是因為可能價格相等)
                 int holdingDiff = prices[i] - holdingSmallest;
                 maxProfit = maxProfit + Math.Max(holdingDiff, 0);
 
@@ -123,5 +123,22 @@ namespace ProblemSolveConsole.LeetCode
 
             return maxProfit;
         }
+
+        //我的方法雖然是Kadane's Algorithm，但另一個角度來說我好像弄得太複雜了
+        //因為在無線交易次數底下且當天能再買進，還沒手續費，我只要每個prices[i] < prices[i+1]都交易，就必定可以收穫全部收益XD
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solutions/208241/explanation-for-the-dummy-like-me-by-chi-5v10/?page=3
+        //這位就是抓住全部收益的方法，如果sell = prices[i+1]; 然後多一次i++後，甚至可以不用第三個while，不過好像就不是dp，只是單純array處理
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solutions/803206/pythonjsjavagoc-on-by-dp-greedy-visualiz-bzy0/?page=3
+        //哇喔，這位是從概念上對我方法優化的降維打擊，是從概念上DP，就是理解
+        //either keep hold, or buy in stock today at stock price
+        //hold = Math.max(prevHold, prevNotHold - stockPrice);
+        // either keep not-hold, or sell out stock today at stock price
+        //notHold = Math.max(prevNotHold, prevHold + stockPrice);
+        // maximum profit must be in not-hold state => 最後一定收回錢賺最多
+        //跟0123BestTimetoBuyandSellStockIII，理解到意思就會形成最佳解...
+
+        //但Kadane's Algorithm還是在這能用啦 => 不過我一直無腦多算cur，所以會明顯比別人多慢一節
+        //第一位方法用的人最多(最單純)，但我覺得第二位的方法最聰明
+
     }
 }
