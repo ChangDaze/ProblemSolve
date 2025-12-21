842c58535badee2ff9316e6d73e5c1c247921a94
ChangDaze <explision000@gmail.com>
2025-12-22 01:15:13 +0800

    [main][feat]1929ConcatenationofArray

---
+++ ProblemSolveConsole/LeetCode/1929ConcatenationofArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1929ConcatenationofArray
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int len = nums.Length + nums.Length;
+            int[] result = new int[len];
+
+            for(int i = 0; i < len; i++)
+            {
+                if (i >= nums.Length) //擴展到n倍可能用%nums.Length比較簡潔
+                {
+                    result[i] = nums[i - nums.Length];
+                }
+                else
+                {
+                    result[i] = nums[i];
+                }
+            }
+
+            return result;
+        }
+
+        //https://leetcode.com/problems/concatenation-of-array/solutions/1330265/java-4-lines-by-vikrant_pc-ysea/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+        //...我還在對題目的簡單沾沾自喜，這個result[i + nums.length] = result[i] = nums[i];直接跑O(n)我還在O(2n)直接被打趴在地上XD
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1929ConcatenationofArrayTest.cs
@@ -0,0 +1,34 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1929ConcatenationofArrayTest
+    {
+
+        private _1929ConcatenationofArray _1929ConcatenationofArray;
+
+        public _1929ConcatenationofArrayTest()
+        {
+            _1929ConcatenationofArray = new _1929ConcatenationofArray();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _1929ConcatenationofArray.GetConcatenation([1, 2, 1]);
+            Assert.Equal([1, 2, 1, 1, 2, 1], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _1929ConcatenationofArray.GetConcatenation([1, 3, 2, 1]);
+            Assert.Equal([1, 3, 2, 1, 1, 3, 2, 1], result);
+        }
+    }
+}
