17bf715e8ebacdccb7b8ca3fe9d10be0923bdc1c
ChangDaze <explision000@gmail.com>
2025-12-23 06:15:42 +0800

    [main][feat]1470ShuffletheArray

---
+++ ProblemSolveConsole/LeetCode/1470ShuffletheArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1470ShuffletheArray
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+
+            for(int i = 0, j = 0; i < n; i++, j+=2)
+            {
+                result[j] = nums[i];
+                result[j + 1] = nums[i+n];
+            }
+
+            return result;
+        }
+
+        //一開始糾結很久能不能不額外用記憶體，最後放棄但想到twopointer
+        //https://leetcode.com/problems/shuffle-the-array/solutions/675956/in-place-on-time-o1-space-with-explanati-373y/
+        //這個人很有趣的完成O(1) in place，但他是套用題目限制，他用int+bit mask (後面n個)一次存兩個數值，先loop一次存完後，array裡面的值在第二次就都能取代了
+        //然後還是有用twopointer因為執行順序還是要twopointer支援
+        //https://leetcode.com/problems/shuffle-the-array/solutions/674309/java-straightforward-1-loop-by-hobiter-2n0k/
+        //這位方法跟我一樣
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1470ShuffletheArrayTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1470ShuffletheArrayTest
+    {
+        private _1470ShuffletheArray _1470ShuffletheArray;
+
+        public _1470ShuffletheArrayTest()
+        {
+            _1470ShuffletheArray = new _1470ShuffletheArray();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _1470ShuffletheArray.Shuffle([2, 5, 1, 3, 4, 7], 3);
+            Assert.Equal([2, 3, 5, 4, 1, 7], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _1470ShuffletheArray.Shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4);
+            Assert.Equal([1, 4, 2, 3, 3, 2, 4, 1], result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] result = _1470ShuffletheArray.Shuffle([1, 1, 2, 2], 2);
+            Assert.Equal([1, 2, 1, 2], result);
+        }
+    }
+}
