8e8c74185993f15931a45aeb6347ee1fbfce2d67
ChangDaze <explision000@gmail.com>
2025-12-24 05:19:01 +0800

    [main][feat]0485MaxConsecutiveOnes

---
+++ ProblemSolveConsole/LeetCode/0485MaxConsecutiveOnes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0485MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            //Time complexity:O(n)
+            //Space complexity:O(1)
+            int max = 0;
+            int streaks = 0;
+
+            foreach (int num in nums)
+            {
+                if(num == 1)
+                {
+                    streaks++;
+                    if(streaks > max)
+                    {
+                        max = streaks;
+                    }
+                }
+                else
+                {
+                    streaks = 0;
+                }
+            }
+
+            return max;
+        }
+
+        //https://leetcode.com/problems/max-consecutive-ones/solutions/96715/easy-java-solution-by-shawngao-ybr7/
+        //大家解法差不多
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0485MaxConsecutiveOnesTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0485MaxConsecutiveOnesTest
+    {
+        private _0485MaxConsecutiveOnes _0485MaxConsecutiveOnes;
+
+        public _0485MaxConsecutiveOnesTest()
+        {
+            _0485MaxConsecutiveOnes = new _0485MaxConsecutiveOnes();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _0485MaxConsecutiveOnes.FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _0485MaxConsecutiveOnes.FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
+            Assert.Equal(3, result);
+        }
+    }
+}
