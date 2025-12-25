79287a74587200a5e1d3d5766dcfeaf06d83d60e
ChangDaze <explision000@gmail.com>
2025-12-25 18:32:43 +0800

    [main][feat]1365HowManyNumbersAreSmallerThantheCurrentNumber

---
+++ ProblemSolveConsole/LeetCode/1365HowManyNumbersAreSmallerThantheCurrentNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1365HowManyNumbersAreSmallerThantheCurrentNumber
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {            
+            int[] result = new int[nums.Length];
+            Array.Copy(nums, result, nums.Length);//O(n)            
+            Dictionary<int, int> record = new Dictionary<int, int>();
+
+            Array.Sort(result);//O(nlogn)
+            //O(n)
+            for (int i = result.Length - 1, j = result.Length - 2; j >= 0; j--) //j會主動前進而已，i負責追上j，用排序好的result找
+            {
+                while (j >= 0 && result[i] == result[j])
+                {
+                    j--;
+                }
+
+                if( j == -1) //剩下的都相同，沒比任何數字大
+                {
+                    break;
+                }
+
+                record[result[i]] = j + 1;
+                i = j;
+            }
+
+            //O(n)
+            for(int i = 0; i<nums.Length; i++)//用原排序的nums更新
+            {
+                if (record.ContainsKey(nums[i]))
+                {
+                    result[i] = record[nums[i]];
+                }
+                else
+                {
+                    result[i] = 0;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1365HowManyNumbersAreSmallerThantheCurrentNumberTest.cs
@@ -0,0 +1,42 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1365HowManyNumbersAreSmallerThantheCurrentNumberTest
+    {
+
+
+        private _1365HowManyNumbersAreSmallerThantheCurrentNumber _1365HowManyNumbersAreSmallerThantheCurrentNumber;
+
+        public _1365HowManyNumbersAreSmallerThantheCurrentNumberTest()
+        {
+            _1365HowManyNumbersAreSmallerThantheCurrentNumber = new _1365HowManyNumbersAreSmallerThantheCurrentNumber();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _1365HowManyNumbersAreSmallerThantheCurrentNumber.SmallerNumbersThanCurrent([8, 1, 2, 2, 3]);
+            Assert.Equal([4, 0, 1, 1, 3], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _1365HowManyNumbersAreSmallerThantheCurrentNumber.SmallerNumbersThanCurrent([6, 5, 4, 8]);
+            Assert.Equal([2, 1, 0, 3], result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] result = _1365HowManyNumbersAreSmallerThantheCurrentNumber.SmallerNumbersThanCurrent([7, 7, 7, 7]);
+            Assert.Equal([0, 0, 0, 0], result);
+        }
+    }
+}
