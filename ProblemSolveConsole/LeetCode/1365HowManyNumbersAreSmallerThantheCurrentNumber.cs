5d733c53c1e58f825d09d94c596bd4b857d52e0a
ChangDaze <explision000@gmail.com>
2026-01-20 05:51:30 +0800

    [main][feat]1365HowManyNumbersAreSmallerThantheCurrentNumber解析

--- ProblemSolveConsole/LeetCode/1365HowManyNumbersAreSmallerThantheCurrentNumber.cs
+++ ProblemSolveConsole/LeetCode/1365HowManyNumbersAreSmallerThantheCurrentNumber.cs
@@ -11,7 +11,7 @@ namespace ProblemSolveConsole.LeetCode
         public int[] SmallerNumbersThanCurrent(int[] nums)
         {            
             int[] result = new int[nums.Length];
-            Array.Copy(nums, result, nums.Length);//O(n)            
+            Array.Copy(nums, result, nums.Length);//O(n)，因為結果要follow原本題目的排序，所以要另外copy            
             Dictionary<int, int> record = new Dictionary<int, int>();
 
             Array.Sort(result);//O(nlogn)
@@ -47,5 +47,12 @@ namespace ProblemSolveConsole.LeetCode
 
             return result;
         }
+
+        //https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/solutions/524996/java-beats-100-on-by-equ1n0x-gsla/
+        //這位直接用0 <= nums[i] <= 100的限制使用int[101]來計數，然後直接 count[i] += count[i-1];來讓福砸度的上下限變化限制住，所以也不用sort，下次可以先想想
+        //https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/solutions/535421/java-clean-hashmap-solution-with-explana-sgq0/
+        //這個人方法跟我一樣，不過他做得比我好，我還呆呆用two pointer，他直接ascending其實index就是要的答案@@
+        //https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/solutions/819148/python-3-9111-faster-52ms-time-explanati-oj0m/
+        //這位方法跟第二位一樣
     }
 }
