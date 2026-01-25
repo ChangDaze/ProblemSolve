39632e2c2d6a19bd67edd7dcb385f47888ec9574
ChangDaze <explision000@gmail.com>
2026-01-25 13:51:27 +0800

    [main][feat]1984MinimumDifferenceBetweenHighestandLowestofKScores

---
+++ ProblemSolveConsole/LeetCode/1984MinimumDifferenceBetweenHighestandLowestofKScores.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1984MinimumDifferenceBetweenHighestandLowestofKScores
+    {
+#if false //舊方法 方法跟新法法其實一樣sliding window
+        public int MinimumDifference(int[] nums, int k)
+        {
+            Array.Sort(nums);
+
+            if (k == 1)
+                return 0;
+
+            int min = int.MaxValue;
+
+            for (int i = k - 1; i < nums.Length; i++)
+            {
+                if (nums[i] - nums[i - k + 1] < min)
+                    min = nums[i] - nums[i - k + 1];
+            }
+
+            return min;
+        }
+#endif
+        public int MinimumDifference(int[] nums, int k)
+        {
+            //這題是要抓出K個，然後用K個中最高和最低能達成min的答案 => 要找到造成diff最小的K個數字
+
+            Array.Sort(nums); //讓數字一定緊貼最近的數字 O(nlogn)
+            int result = int.MaxValue;
+            for(int i = 0, j = k - 1; j < nums.Length; i++, j++)
+            {
+                if (nums[j] - nums[i] < result)//感覺也能造個temp直接不用if每次temp + nums[j]- nums[i]- nums[i]之類的
+                {
+                    result = nums[j] - nums[i];
+                }
+            }
+            return result;
+        }
+        //這題一開始一直沒看懂題目，要多練一下題目理解，後來試問AI知道是用sliding window
+        //https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/solutions/1432024/sort-and-skip-by-votrubac-7tmd/
+        //大家方法其實都一樣 sort + slifing window
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1984MinimumDifferenceBetweenHighestandLowestofKScoresTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1984MinimumDifferenceBetweenHighestandLowestofKScoresTest
+    {
+        private _1984MinimumDifferenceBetweenHighestandLowestofKScores _1984MinimumDifferenceBetweenHighestandLowestofKScores;
+
+        public _1984MinimumDifferenceBetweenHighestandLowestofKScoresTest()
+        {
+            _1984MinimumDifferenceBetweenHighestandLowestofKScores = new _1984MinimumDifferenceBetweenHighestandLowestofKScores();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1984MinimumDifferenceBetweenHighestandLowestofKScores.MinimumDifference([90], 1);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1984MinimumDifferenceBetweenHighestandLowestofKScores.MinimumDifference([9, 4, 1, 7], 2);
+            Assert.Equal(2, result);
+        }
+    }
+}
