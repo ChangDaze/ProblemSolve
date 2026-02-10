e03cdbb6e94b919869b817972b02df9063ea49b9
ChangDaze <explision000@gmail.com>
2026-02-11 05:15:55 +0800

    [main][feat]0373FindKPairswithSmallestSums

---
+++ ProblemSolveConsole/LeetCode/0373FindKPairswithSmallestSums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0373FindKPairswithSmallestSums
+    {
+        public IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
+        {
+            //Time complexity: O(N * M * log K) => pq限制k也有好處，比較peek時只有O(1)，因為容量只有k，所以新增最多也就O(log K)，不限制容量logk也是會很大
+            //Space complexity: O(K) => 因為容量只有k
+
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if(pq.Count < k)
+                    {
+                        pq.Enqueue([nums1[i], nums2[j]], -(nums1[i] + nums2[j]));
+                    }
+                    else
+                    {
+                        pq.TryPeek(out int[] element, out int priority);
+                        if (-(nums1[i] + nums2[j]) <= priority) //<=最小值就剪枝，基本上所有組合都有可能...，沒剪枝就會超過timelimit
+                        {
+                            break;
+                        }
+
+                        pq.DequeueEnqueue([nums1[i], nums2[j]], -(nums1[i] + nums2[j])); //沒dequeue限制space上限就會超過space limit
+                    }
+                }
+            }
+
+            List<IList<int>> result = new List<IList<int>>();
+            
+            for (int i =0; i < k && pq.Count > 0; i++)
+            {
+                result.Add(pq.Dequeue());
+            }
+            
+            return result ;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0373FindKPairswithSmallestSumsTest.cs
@@ -0,0 +1,52 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0373FindKPairswithSmallestSumsTest
+    {
+        private _0373FindKPairswithSmallestSums _0373FindKPairswithSmallestSums;
+
+        public _0373FindKPairswithSmallestSumsTest()
+        {
+            _0373FindKPairswithSmallestSums = new _0373FindKPairswithSmallestSums();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            IList<IList<int>> result = _0373FindKPairswithSmallestSums.KSmallestPairs([1, 7, 11], [2, 4, 6], 3);
+            Assert.Equal(Normalize([[1, 2], [1, 4], [1, 6]]), Normalize(result));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            IList<IList<int>> result = _0373FindKPairswithSmallestSums.KSmallestPairs([1, 1, 2], [1, 2, 3], 2);
+            Assert.Equal(Normalize([[1, 1], [1, 1]]), Normalize(result));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            IList<IList<int>> result = _0373FindKPairswithSmallestSums.KSmallestPairs([1, 2, 4, 5, 6], [3, 5, 7, 9], 20);
+            Assert.Equal(Normalize([[1, 3], [2, 3], [1, 5], [2, 5], [4, 3], [1, 7], [5, 3], [2, 7], [4, 5], [6, 3], [1, 9], [5, 5], [2, 9], [4, 7], [6, 5], [5, 7], [4, 9], [6, 7], [5, 9], [6, 9]]), Normalize(result));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            IList<IList<int>> result = _0373FindKPairswithSmallestSums.KSmallestPairs([1, 2, 4, 5, 6], [3, 5, 7, 9], 3);
+            Assert.Equal(Normalize([[1, 3], [2, 3], [1, 5]]), Normalize(result));
+        }
+
+        private string Normalize(IList<IList<int>> list) =>
+        string.Join(";", list
+            .Select(inner => string.Join(",", inner.OrderBy(x => x)))
+            .OrderBy(s => s));
+    }
+}
