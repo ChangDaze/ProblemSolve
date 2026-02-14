66b7f808dca4392c10f6b3221d00a810f3af0d18
ChangDaze <explision000@gmail.com>
2026-02-14 13:19:33 +0800

    [main][feat]0263UglyNumber

---
+++ ProblemSolveConsole/LeetCode/0263UglyNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0263UglyNumber
+    {
+        public bool IsUgly(int n)
+        {
+            //Time complexity:O(logn)
+            //Space complexity:O(1)O(1)
+            if (n == 0) return false;//題目會出0，防呆，沒防會無限執行
+
+            //醜數 Ugly Number: 正整數，質因數只有2,3,5 An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+            //只是用作因數分解練習和概念，醜數本身是設計出來的
+            int[] primes = { 2, 3, 5 };
+
+            foreach (int i in primes)//prime factor質因數
+            {
+                while (n % i == 0)//代表可整除
+                {
+                    n /= i; //可能不會是1，但while會繼續判斷同個數可否能除，除到剩1，或其他prime是否為因數
+                }
+            }
+
+            return n == 1;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0263UglyNumberTest.cs
@@ -0,0 +1,47 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0263UglyNumberTest
+    {
+        private _0263UglyNumber _0263UglyNumber;
+
+        public _0263UglyNumberTest()
+        {
+            _0263UglyNumber = new _0263UglyNumber();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            bool result = _0263UglyNumber.IsUgly(6);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            bool result = _0263UglyNumber.IsUgly(1);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            bool result = _0263UglyNumber.IsUgly(14);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            bool result = _0263UglyNumber.IsUgly(0);
+            Assert.False(result);
+        }
+    }
+}
