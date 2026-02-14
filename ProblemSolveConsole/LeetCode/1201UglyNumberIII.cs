6c09e456e08af9e1f25c555ce8e410148ad3187d
ChangDaze <explision000@gmail.com>
2026-02-15 00:13:14 +0800

    [main][feat]1201UglyNumberIII

---
+++ ProblemSolveConsole/LeetCode/1201UglyNumberIII.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1201UglyNumberIII
+    {
+        public int NthUglyNumber(int n, int a, int b, int c)
+        {
+            //time : O(log(high) * log(min(a, b, c)))
+            //space : O(1)
+            long low = 1;
+            long high = long.MaxValue;
+            long ans = high;
+            while (low <= high)
+            {
+                long mid = low + (high - low) /2;
+                if(Count(mid, a, b, c) >= n)
+                {
+                    ans = mid;
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return (int)ans;
+        }
+
+        private long Count(long n,long a, long b, long c)
+        {
+            return (n / a + n / b + n / c)
+                - (n / GetLCM(a, b) + n / GetLCM(b, c) + n / GetLCM(a, c))
+                + (n / GetLCM(GetLCM(a, b), c));
+        }
+
+        private long GetGCD(long a, long b)
+        {
+            while(b!=0)
+            {
+                a %= b;
+                (a, b) = (b, a);
+            }
+            return a;
+        }
+
+        private long GetLCM(long a, long b)
+        {
+            return (a / GetGCD(a, b)) * b;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1201UglyNumberIIITest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1201UglyNumberIIITest
+    {
+        private _1201UglyNumberIII _1201UglyNumberIII;
+
+        public _1201UglyNumberIIITest()
+        {
+            _1201UglyNumberIII = new _1201UglyNumberIII();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1201UglyNumberIII.NthUglyNumber(3, 2, 3, 5);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1201UglyNumberIII.NthUglyNumber(4, 2, 3, 4);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _1201UglyNumberIII.NthUglyNumber(5, 2, 11, 13);
+            Assert.Equal(10, result);
+        }
+    }
+}
