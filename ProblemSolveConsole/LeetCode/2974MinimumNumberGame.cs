1a16f91688e7e2498ed84991e44dece876ec2ebc
ChangDaze <explision000@gmail.com>
2026-02-14 13:08:39 +0800

    [main][feat]2974MinimumNumberGame

---
+++ ProblemSolveConsole/LeetCode/2974MinimumNumberGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _2974MinimumNumberGame
+    {
+        public int[] NumberGame(int[] nums)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+            foreach(int num in nums)
+            {
+                pq.Enqueue(num, num);
+            }
+
+            List<int> result = new List<int>();
+
+            while(pq.Count > 0)
+            {
+                int alice = pq.Dequeue();
+                int bob = pq.Dequeue();
+
+                result.Add(bob);
+                result.Add(alice);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/2974MinimumNumberGameTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _2974MinimumNumberGameTest
+    {
+        private _2974MinimumNumberGame _2974MinimumNumberGame;
+
+        public _2974MinimumNumberGameTest()
+        {
+            _2974MinimumNumberGame = new _2974MinimumNumberGame();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _2974MinimumNumberGame.NumberGame([5, 4, 2, 3]);
+            Assert.Equal([3, 2, 5, 4], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _2974MinimumNumberGame.NumberGame([2, 5]);
+            Assert.Equal([5, 2], result);
+        }
+    }
+}
