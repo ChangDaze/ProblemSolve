746f4f432463d807b69172c3285d19723f7581b2
ChangDaze <explision000@gmail.com>
2026-02-20 23:51:22 +0800

    [main][feat]0263UglyNumber註解

--- ProblemSolveConsole/LeetCode/0263UglyNumber.cs
+++ ProblemSolveConsole/LeetCode/0263UglyNumber.cs
@@ -12,7 +12,7 @@ namespace ProblemSolveConsole.LeetCode
         {
             //Time complexity:O(logn)
             //Space complexity:O(1)O(1)
-            if (n == 0) return false;//題目會出0，防呆，沒防會無限執行
+            if (n == 0) return false;//題目會出0，防呆，沒防會無限執行，然後0不算正整數
 
             //醜數 Ugly Number: 正整數，質因數只有2,3,5 An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
             //只是用作因數分解練習和概念，醜數本身是設計出來的
@@ -29,4 +29,11 @@ namespace ProblemSolveConsole.LeetCode
             return n == 1;
         }
     }
+
+    //https://leetcode.com/problems/ugly-number/solutions/69214/2-4-lines-every-language-by-stefanpochma-zk0d/
+    //這位方法跟我一樣
+    //https://leetcode.com/problems/ugly-number/solutions/6634497/master-the-prime-factor-shortcut-intervi-fgxq/
+    //這位也一樣
+
+    //看來大家方法都一樣
 }
