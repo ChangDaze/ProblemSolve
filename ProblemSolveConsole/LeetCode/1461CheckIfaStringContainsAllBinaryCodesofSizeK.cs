9e92ecef9ad649c84614ffbfaf2053958db9341a
ChangDaze <explision000@gmail.com>
2026-02-24 06:17:25 +0800

    [main][feat]1461CheckIfaStringContainsAllBinaryCodesofSizeK

---
+++ ProblemSolveConsole/LeetCode/1461CheckIfaStringContainsAllBinaryCodesofSizeK.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1461CheckIfaStringContainsAllBinaryCodesofSizeK
+    {
+        public bool HasAllCodes(string s, int k)
+        {
+            int need = 1 << k;
+            int mask = need - 1;
+            if (s.Length < k + mask)
+            {
+                return false;
+            }
+
+            bool[] seen = new bool[need];
+
+            int count = 0;
+            int hash = 0;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                hash = ((hash << 1) & mask) | (s[i] - '0');
+
+                if(i >= k - 1)
+                {
+                    if (!seen[hash])
+                    {
+                        seen[hash] = true;
+                        count++;
+
+                        if(count == need)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1461CheckIfaStringContainsAllBinaryCodesofSizeKTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1461CheckIfaStringContainsAllBinaryCodesofSizeKTest
+    {
+        private _1461CheckIfaStringContainsAllBinaryCodesofSizeK _1461CheckIfaStringContainsAllBinaryCodesofSizeK;
+
+        public _1461CheckIfaStringContainsAllBinaryCodesofSizeKTest()
+        {
+            _1461CheckIfaStringContainsAllBinaryCodesofSizeK = new _1461CheckIfaStringContainsAllBinaryCodesofSizeK();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            bool result = _1461CheckIfaStringContainsAllBinaryCodesofSizeK.HasAllCodes("00110110", 2);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            bool result = _1461CheckIfaStringContainsAllBinaryCodesofSizeK.HasAllCodes("0110", 1);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            bool result = _1461CheckIfaStringContainsAllBinaryCodesofSizeK.HasAllCodes("0110", 2);
+            Assert.False(result);
+        }
+    }
+}
