f07fb2daa523c20525e3c4db6f1c850eef84d104
ChangDaze <explision000@gmail.com>
2026-02-26 06:21:40 +0800

    [main][feat]1356SortIntegersbyTheNumberof1Bits

---
+++ ProblemSolveConsole/LeetCode/1356SortIntegersbyTheNumberof1Bits.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    internal class _1356SortIntegersbyTheNumberof1Bits
+    {
+#if false
+            public int[] SortByBits(int[] arr)
+            {
+                int[][] record = new int[arr.Length][];
+                for(int i = 0;i<record.Length ; i++)
+                {
+                    record[i] = new int[2] { CountBits(arr[i]), arr[i] };
+                }
+
+                return record.OrderBy(x => x[0]).ThenBy(x => x[1]).Select(x => x[1]).ToArray();
+            }
+
+            public int CountBits(int num)
+            {
+                int count = 0;
+                while (num != 0)
+                {
+                    count += num % 2;//餘1代表這個位數有1
+                    num = num / 2;
+                }
+
+                return count;
+            }
+#endif
+        public int[] SortByBits(int[] arr)
+        {
+            return arr.OrderBy(x => System.Numerics.BitOperations.PopCount((uint)x)).ThenBy(x=>x).ToArray();
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1356SortIntegersbyTheNumberof1BitsTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    internal class _1356SortIntegersbyTheNumberof1BitsTest
+    {
+    }
+}
