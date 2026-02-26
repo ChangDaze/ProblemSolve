d66e2ebd11bb564ec08d733d614d7bef2a6900d3
ChangDaze <explision000@gmail.com>
2026-02-27 01:24:45 +0800

    [main][feat]1356SortIntegersbyTheNumberof1Bits註解

--- ProblemSolveConsole/LeetCode/1356SortIntegersbyTheNumberof1Bits.cs
+++ ProblemSolveConsole/LeetCode/1356SortIntegersbyTheNumberof1Bits.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ProblemSolveConsole.LeetCode
 {
-    internal class _1356SortIntegersbyTheNumberof1Bits
+    public class _1356SortIntegersbyTheNumberof1Bits
     {
-#if false
+#if false //舊方法，那個時候不會bit manipulation，意外用int /2 + math來解，其實蠻聰明的耶，雖然sort語法用得稀爛(要學一下怎麼寫Comparator...)，但單純看實作CountBits我覺得很有創意
             public int[] SortByBits(int[] arr)
             {
                 int[][] record = new int[arr.Length][];
@@ -34,7 +34,27 @@ namespace ProblemSolveConsole.LeetCode
 #endif
         public int[] SortByBits(int[] arr)
         {
+            //time:O(N) (每個元素PopCount) + O(N log N) (排序) + O(N) (toarray) = O(N log N)
+            //space:O(N)
+            //1.by bits order
+            //2.by value order
             return arr.OrderBy(x => System.Numerics.BitOperations.PopCount((uint)x)).ThenBy(x=>x).ToArray();
         }
+
+        //https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/2715364/simple-java-solution-easy-to-understand-1r0yv/
+        //這個很有趣蠻值得學的，也蠻適合面試
+        //1.因為value上限10001，所以把arr[i]+=bit*10001 => 組成成高位排序
+        //2.因為是+=所以包含arr[i]的value，所以同bit也有低位排序
+        //3.一次array sort就能完成高低為排序
+        //4.%10001 把高低位編碼解回原本編碼
+        //5.空間是 inplace會很有效率
+        //=>這種「數值壓縮」的技巧在處理多層級排序、或是想在有限空間內存多個屬性時非常好用
+        //https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/4224485/9658-built-in-functions-brian-kerninghan-wnyp/
+        //這方法基本跟我一樣，不過是手刻的
+        //https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/3986287/sort-integers-by-the-number-of-1-bits-by-h9v1/
+        //這是官方的，索然沒有有趣的解法，但很詳細的教學了bit maniplation
+        //1.和我一樣用API的解法
+        //2.基礎用mask確認bit數的方法 num & mask 判斷、num ^= mask判斷後清理、mask <<= 1準備下個判斷
+        //3.Brian Kerninghan's Algorithm => num &= (num - 1)直到num=0 => 等同bit數 => 這是一個值得背起來的工具之一，因為異動也是非常小
     }
 }
--- ProblemSolveConsoleUnitTest/LeetCode/1356SortIntegersbyTheNumberof1BitsTest.cs
+++ ProblemSolveConsoleUnitTest/LeetCode/1356SortIntegersbyTheNumberof1BitsTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProblemSolveConsole.LeetCode;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace ProblemSolveConsoleUnitTest.LeetCode
 {
-    internal class _1356SortIntegersbyTheNumberof1BitsTest
+    public class _1356SortIntegersbyTheNumberof1BitsTest
     {
+        private _1356SortIntegersbyTheNumberof1Bits _1356SortIntegersbyTheNumberof1Bits;
+
+        public _1356SortIntegersbyTheNumberof1BitsTest()
+        {
+            _1356SortIntegersbyTheNumberof1Bits = new _1356SortIntegersbyTheNumberof1Bits();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _1356SortIntegersbyTheNumberof1Bits.SortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+            Assert.Equal([0, 1, 2, 4, 8, 3, 5, 6, 7], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _1356SortIntegersbyTheNumberof1Bits.SortByBits([1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]);
+            Assert.Equal([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024], result);
+        }
     }
 }
