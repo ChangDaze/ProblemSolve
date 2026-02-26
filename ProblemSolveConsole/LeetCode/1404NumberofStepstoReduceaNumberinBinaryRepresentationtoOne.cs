14091b2c0ccb9ec2a93d289348981fbf40d51202
ChangDaze <explision000@gmail.com>
2026-02-26 20:26:41 +0800

    [main][feat]1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne

---
+++ ProblemSolveConsole/LeetCode/1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne
+    {
+        public int NumSteps(string s)
+        {
+            int step = 0;
+            LinkedList<char> chars = new LinkedList<char>(s.ToCharArray());
+            while (chars.Count > 1)
+            {
+
+                if (chars.Last() == '0')
+                {
+                    step++;
+                }
+                else
+                {
+                    LinkedListNode<char> current = chars.Last;
+
+                    while (current != null)
+                    {
+                        if (current.Value == '0')
+                        {
+                            current.Value = '1';
+                            break;
+                        }
+                        else
+                        {
+                            current.Value = '0';
+                        }
+
+                        current = current.Previous;
+                    }
+
+                    if (current == null)
+                    {
+                        chars.AddFirst('1');
+                    }
+
+                    step += 2;
+                }
+                chars.RemoveLast();                
+            }
+            return step;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOneTest.cs
@@ -0,0 +1,26 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOneTest
+    {
+        private _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne;
+
+        public _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOneTest()
+        {
+            _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne = new _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1404NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.NumSteps("11000");
+            Assert.Equal(6, result);
+        }
+    }
+}
