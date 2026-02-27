dd24f7212614b053b9d47f7a2794530277d41b7e
ChangDaze <explision000@gmail.com>
2026-02-27 13:15:42 +0800

    [main][feat]1022SumofRootToLeafBinaryNumbers註解

--- ProblemSolveConsole/LeetCode/1022SumofRootToLeafBinaryNumbers.cs
+++ ProblemSolveConsole/LeetCode/1022SumofRootToLeafBinaryNumbers.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProblemSolveConsole.LeetCode
 {
-    internal class _1022SumofRootToLeafBinaryNumbers
+    public class _1022SumofRootToLeafBinaryNumbers
     {
         public class TreeNode
         {
@@ -20,7 +20,7 @@ namespace ProblemSolveConsole.LeetCode
                 this.right = right;
             }
         }
-#if false
+#if false //舊方法，這是以前還不熟bit時的做法，就自己做轉換器和用靜態加總，其實蠻粗糙好懂得，就是過程可能比較耗時間，複雜度其實一樣
         public double result = 0;
         public int SumRootToLeaf(TreeNode root)
         {
@@ -52,22 +52,35 @@ namespace ProblemSolveConsole.LeetCode
 #endif
         public int SumRootToLeaf(TreeNode root)
         {
+            //time:O(n)
+            //space:O(n) => function stack
             return Recursive(root, 0);
         }
 
         public int Recursive(TreeNode node, int cur)
         {
-            if (node == null) return 0;
+            if (node == null) return 0;//會遇到單邊沒有child的node，可以用0模擬不存在的child
 
-            cur <<= 1;
+            cur <<= 1;//DFS到leaf並計算沿途得leaf傳下去
             cur |= node.val;
 
-            if (node.left == null && node.right == null)
+            if (node.left == null && node.right == null) //leaf 是兩邊都沒有child的node
             {
                 return cur;
             }
 
-            return Recursive(node.left, cur) + Recursive(node.right, cur);
+            return Recursive(node.left, cur) + Recursive(node.right, cur);//答案是DFS到末端的加總
         }
+
+        //https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/solutions/270025/javacpython-recursive-solution-by-lee215-fcw2/
+        //這位解法其實跟我一樣，只是把bit操作隱藏在*2中
+        //1.root.left == root.right => 單純簡短 root.left == null && root.right == null，因為是記憶體所以有值就不會相等
+        //2.val = val * 2 + root.val; => val * 2就是把傳入的值做left shift => 其實也蠻值得記住的，算是第二種表現bit的方法
+        //https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/solutions/1681682/well-detailed-explaination-java-c-python-mqkc/
+        //這位方法跟第一位一樣
+        //https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/solutions/270600/java-simple-dfs-by-gcarrillo-w0yz/
+        //這位方法則跟我一樣用left shift
+        //看來標準解法確定有兩種表現方式 1. *2 2. left shift
+
     }
 }
--- ProblemSolveConsoleUnitTest/LeetCode/1022SumofRootToLeafBinaryNumbersTest.cs
+++ ProblemSolveConsoleUnitTest/LeetCode/1022SumofRootToLeafBinaryNumbersTest.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using ProblemSolveConsole.LeetCode;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ProblemSolveConsole.LeetCode._1022SumofRootToLeafBinaryNumbers;
 
 namespace ProblemSolveConsoleUnitTest.LeetCode
 {
-    internal class _1022SumofRootToLeafBinaryNumbersTest
+    public class _1022SumofRootToLeafBinaryNumbersTest
     {
+        private _1022SumofRootToLeafBinaryNumbers _1022SumofRootToLeafBinaryNumbers;
 
+        public _1022SumofRootToLeafBinaryNumbersTest()
+        {
+            _1022SumofRootToLeafBinaryNumbers = new _1022SumofRootToLeafBinaryNumbers();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1022SumofRootToLeafBinaryNumbers.SumRootToLeaf(
+                new TreeNode(1,
+                    new TreeNode(0,
+                        new TreeNode(0),
+                        new TreeNode(1)),
+                    new TreeNode(1,
+                        new TreeNode(0),
+                        new TreeNode(1))
+                )
+            );
+            Assert.Equal(22, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1022SumofRootToLeafBinaryNumbers.SumRootToLeaf(
+                new TreeNode(0)
+            );
+            Assert.Equal(0, result);
+        }
     }
 }
