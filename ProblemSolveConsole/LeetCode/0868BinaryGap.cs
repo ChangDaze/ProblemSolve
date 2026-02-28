419f25bb29d6ea6df262cea1a52efd495d6a554a
ChangDaze <explision000@gmail.com>
2026-02-28 16:32:09 +0800

    [main][feat]0868BinaryGap註解

---
+++ ProblemSolveConsole/LeetCode/0868BinaryGap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0868BinaryGap
+    {
+        public int BinaryGap(int n)
+        {
+            //Time complexity:O(n)
+            //Space complexity:O(1)
+            int max = 0;
+            int pointer = -1;
+            int previousBit = int.MaxValue;
+
+            //判斷每個bit是否為1，使用right shift推進
+            while (n != 0)
+            {
+                pointer++;
+                if( (n&1) == 1)//檢查到此bit是1
+                {
+                    int gap = pointer - previousBit;//檢查距離
+                    if(gap > max)//更新最遠距離
+                    {
+                        max = gap;
+                    }
+                    previousBit = pointer;//更新前次位置的紀錄
+                }
+                n >>= 1;
+            }
+
+            return max;
+        }
+
+        //https://leetcode.com/problems/binary-gap/solutions/149835/cjavapython-dividing-by-2-by-lee215-wq4o/
+        //這位方法原理其實跟我一樣，只是他表現出來不同
+        //1.N % 2 == 1 => n&1
+        //2.N /= 2 => n >>= 1
+        //3.int d = -32;d++;d = 0 =>就是像previousBit的錨點而已
+        //https://leetcode.com/problems/binary-gap/description/
+        //這位方法其實跟第一位一樣，他有maxGap + 1是因為他的gap是用+1算的，不是用兩點相減算的
+        //https://leetcode.com/problems/binary-gap/solutions/149820/binary-gap-by-leetcode-qn2i/
+        //這方法其實跟我的方法一樣，不過它寫成一團我覺得會難看懂一點，比較意外的是他把bit shift看作O(logN) @@
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0868BinaryGapTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0868BinaryGapTest
+    {
+        private _0868BinaryGap _0868BinaryGap;
+
+        public _0868BinaryGapTest()
+        {
+            _0868BinaryGap = new _0868BinaryGap();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _0868BinaryGap.BinaryGap(22);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _0868BinaryGap.BinaryGap(8);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _0868BinaryGap.BinaryGap(5);
+            Assert.Equal(2, result);
+        }
+    }
+}
