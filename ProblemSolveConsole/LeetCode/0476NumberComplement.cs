158555b5e98945c40d60ebd6282cfcdd867ced6b
ChangDaze <explision000@gmail.com>
2026-03-04 01:58:08 +0800

    [main][feat]0476NumberComplement

---
+++ ProblemSolveConsole/LeetCode/0476NumberComplement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0476NumberComplement
+    {
+        public int FindComplement(int num)
+        {
+            int result = 0;
+            int pow = 0;
+            while (num > 0)
+            {
+                if(!((num & 1) == 1))
+                {
+                    result += (int)Math.Pow(2, pow);
+                }
+                
+                num >>= 1;
+                pow++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0476NumberComplementTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0476NumberComplementTest
+    {
+        private _0476NumberComplement _0476NumberComplement;
+
+        public _0476NumberComplementTest()
+        {
+            _0476NumberComplement = new _0476NumberComplement();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _0476NumberComplement.FindComplement(5);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _0476NumberComplement.FindComplement(1);
+            Assert.Equal(0, result);
+        }
+    }
+}
