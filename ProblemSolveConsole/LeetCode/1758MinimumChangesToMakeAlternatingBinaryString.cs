5a58113777b0bb3f31bff8af329a45c0a5b60cf2
ChangDaze <explision000@gmail.com>
2026-03-06 05:46:58 +0800

    [main][feat]1758MinimumChangesToMakeAlternatingBinaryString

---
+++ ProblemSolveConsole/LeetCode/1758MinimumChangesToMakeAlternatingBinaryString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1758MinimumChangesToMakeAlternatingBinaryString
+    {
+        public int MinOperations(string s)
+        {
+            char[] firstZero = s.ToCharArray(); //偶數0 奇數1 正確
+            int firstZeroCount = 0;
+            if (firstZero[0] != '0') {
+                firstZero[0] = '0';
+                firstZeroCount++; 
+            }
+            char[] firstOne = s.ToCharArray(); //偶數1 奇數0 正確
+            int firstOneCount = 0;
+            if (firstOne[0] != '1') {
+                firstOne[0] = '1';
+                firstOneCount++; 
+            }
+            
+            for (int i = 1; i < s.Length; i++)
+            {
+                if(firstZero[i] == firstZero[i - 1])
+                {
+                    firstZero[i] = firstZero[i - 1] == '1' ? '0' : '1';
+                    firstZeroCount++;
+                }
+
+                if (firstOne[i] == firstOne[i - 1])
+                {
+                    firstOne[i] = firstOne[i - 1] == '1' ? '0' : '1';
+                    firstOneCount++;
+                }
+            }
+
+            return Math.Min(firstZeroCount, firstOneCount);
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1758MinimumChangesToMakeAlternatingBinaryStringTest.cs
@@ -0,0 +1,63 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1758MinimumChangesToMakeAlternatingBinaryStringTest
+    {
+        private _1758MinimumChangesToMakeAlternatingBinaryString _1758MinimumChangesToMakeAlternatingBinaryString;
+
+        public _1758MinimumChangesToMakeAlternatingBinaryStringTest()
+        {
+            _1758MinimumChangesToMakeAlternatingBinaryString = new _1758MinimumChangesToMakeAlternatingBinaryString();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("0100");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("10");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("1111");
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("10010100");
+            Assert.Equal(3, result);
+        }
+
+
+        [Fact]
+        public void Test5()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("0000000000");
+            Assert.Equal(5, result);
+        }
+
+
+        [Fact]
+        public void Test6()
+        {
+            int result = _1758MinimumChangesToMakeAlternatingBinaryString.MinOperations("0000111");
+            Assert.Equal(3, result);
+        }
+    }
+}
