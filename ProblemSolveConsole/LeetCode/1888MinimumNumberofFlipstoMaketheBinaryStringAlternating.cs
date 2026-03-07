21373c4b8b9ab4fbab9c441f8d3856ae7d92fc90
ChangDaze <explision000@gmail.com>
2026-03-08 03:36:05 +0800

    [main][feat]1888MinimumNumberofFlipstoMaketheBinaryStringAlternating

---
+++ ProblemSolveConsole/LeetCode/1888MinimumNumberofFlipstoMaketheBinaryStringAlternating.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating
+    {
+        public int MinFlips(string s)
+        {
+            int len = s.Length;
+            int zeroFirst = 0;//match
+            int oneFirst = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if(i%2 == 0)
+                {
+                    if (s[i] == '0')
+                    {
+                        zeroFirst++;
+                    }
+                    else
+                    {
+                        oneFirst++;
+                    }
+                }
+                else
+                {
+                    if (s[i] == '0')
+                    {
+                        oneFirst++;                        
+                    }
+                    else
+                    {
+                        zeroFirst++;
+                    }
+                }
+            }
+
+            int max = Math.Max(zeroFirst, oneFirst);
+            bool lastEven = (len - 1)%2 == 0;
+
+            for (int i = 0; i < len; i++)//指到的位置從0變到Last
+            {
+                zeroFirst = len - zeroFirst;
+                oneFirst = len - oneFirst;
+
+                if (s[i] == '1')// index 0 > index -1, even to odd
+                {
+                    zeroFirst--; //zeroFirst odd時是'1'match               
+                }
+                else
+                {
+                    oneFirst--; //oneFirst odd時是'0'match
+                }
+
+                if (lastEven)
+                {
+                    if (s[i] == '0')
+                    {
+                        zeroFirst++;
+                    }
+                    else
+                    {
+                        oneFirst++;
+                    }
+                }
+                else
+                {
+                    if (s[i] == '0')
+                    {
+                        oneFirst++;
+                    }
+                    else
+                    {
+                        zeroFirst++;
+                    }
+                }
+
+                max = Math.Max(Math.Max(max,zeroFirst), oneFirst);
+            }
+
+            return len - max;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1888MinimumNumberofFlipstoMaketheBinaryStringAlternatingTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1888MinimumNumberofFlipstoMaketheBinaryStringAlternatingTest
+    {
+        private _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating;
+
+        public _1888MinimumNumberofFlipstoMaketheBinaryStringAlternatingTest()
+        {
+            _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating = new _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating.MinFlips("111000");
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating.MinFlips("010");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _1888MinimumNumberofFlipstoMaketheBinaryStringAlternating.MinFlips("1110");
+            Assert.Equal(1, result);
+        }
+    }
+}
