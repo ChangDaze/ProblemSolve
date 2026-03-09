427b31aaf9333d5918f4204412fcd8d870451ef3
ChangDaze <explision000@gmail.com>
2026-03-10 06:29:08 +0800

    [main][feat]3129FindAllPossibleStableBinaryArraysI

---
+++ ProblemSolveConsole/LeetCode/3129FindAllPossibleStableBinaryArraysI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _3129FindAllPossibleStableBinaryArraysI
+    {
+        public int NumberOfStableArrays(int zero, int one, int limit)
+        {
+            int mod = 1000000007;
+
+            long[][][] dp = new long[zero + 1][][];
+            //long[zero + 1][one + 1][2];
+            //index 1 0 ~ zero
+            //index 2 0 ~ one 
+            //index 3 最後是 append 0 還是 1
+            //ex : bp[i][j][0] => 最後是0的情況下有i個0,j個1的所有組合
+
+            for (int i = 0; i <= zero; i++)
+            {
+                dp[i] = new long[one + 1][];
+                for(int j = 0; j <= one; j++)
+                {
+                    dp[i][j] = new long[2];
+                }
+            }
+
+            for(int i = 1; i <= Math.Min(zero, limit); i++)
+            {
+                dp[i][0][0] = 1;
+            }
+
+            for (int j = 1; j <= Math.Min(one, limit); j++)
+            {
+                dp[0][j][1] = 1;
+            }
+
+            for (int z = 1; z <= zero; z++)
+            {
+                for (int o = 1; o <= one; o++)
+                {
+                    dp[z][o][0] = (dp[z-1][o][0] + dp[z - 1][o][1]) % mod;
+
+                    if(z-limit-1 >= 0)
+                    {
+                        dp[z][o][0] = (dp[z][o][0] - dp[z - limit - 1][o][1] + mod) % mod;
+                    }
+
+                    dp[z][o][1] = (dp[z][o-1][0] + dp[z][o-1][1]) % mod;
+
+                    if (o - limit - 1 >= 0)
+                    {
+                        dp[z][o][1] = (dp[z][o][1] - dp[z][o - limit - 1][0] + mod) % mod;
+                    }
+                }
+            }
+
+            return (int)((dp[zero][one][0] + dp[zero][one][1] + mod) % mod);
+        }
+
+        //https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/solutions/7635866/dynamic-programming-approach-to-count-st-wkw9/?envType=daily-question&envId=2026-03-09
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/3129FindAllPossibleStableBinaryArraysITest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _3129FindAllPossibleStableBinaryArraysITest
+    {
+        private _3129FindAllPossibleStableBinaryArraysI _3129FindAllPossibleStableBinaryArraysI;
+
+        public _3129FindAllPossibleStableBinaryArraysITest()
+        {
+            _3129FindAllPossibleStableBinaryArraysI = new _3129FindAllPossibleStableBinaryArraysI();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _3129FindAllPossibleStableBinaryArraysI.NumberOfStableArrays(1, 1, 2);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _3129FindAllPossibleStableBinaryArraysI.NumberOfStableArrays(1, 2, 1);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _3129FindAllPossibleStableBinaryArraysI.NumberOfStableArrays(3, 3, 2);
+            Assert.Equal(14, result);
+        }
+    }
+}
