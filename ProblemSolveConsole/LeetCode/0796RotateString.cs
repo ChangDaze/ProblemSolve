29146f07c116d1dd0b2a491b67c415719b9d14bc
ChangDaze <explision000@gmail.com>
2026-03-11 01:21:01 +0800

    [main][feat]0796RotateString

---
+++ ProblemSolveConsole/LeetCode/0796RotateString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0796RotateString
+    {
+#if false //舊方法
+    public bool RotateString(string A, string B) {
+        
+        if(A.Length != B.Length)
+            return false;
+        if(A == "" && B =="")
+            return true;
+       
+        int k = 0;
+        while(k < A.Length)
+        {
+            char[] temp = new char[A.Length];
+            for(int i = 0; i < A.Length; i++)
+                temp[i] = A[(i + A.Length - k) % A.Length]; // left shift
+            
+            string str = new string(temp);
+            if(str == B)
+                return true;
+            
+            k++;
+        }
+        
+        return false;
+    }
+#endif
+        public bool RotateString(string s, string goal)
+        {
+            if(s.Length != goal.Length) return false; //因為目標是rotate所以原始長度一定要相同
+
+            string ss = s[1..] + s;//C# 新有的字串切片寫法
+            return ss.Contains(goal);
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0796RotateStringTest.cs
@@ -0,0 +1,41 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0796RotateStringTest
+    {
+        private _0796RotateString _0796RotateString;
+
+        public _0796RotateStringTest()
+        {
+            _0796RotateString = new _0796RotateString();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            bool result = _0796RotateString.RotateString("abcde", "cdeab");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            bool result = _0796RotateString.RotateString("abcde", "abced");
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+            bool result = _0796RotateString.RotateString("aa", "a");
+            Assert.False(result);
+        }
+    }
+}
