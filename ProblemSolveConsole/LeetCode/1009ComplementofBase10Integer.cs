0b0f9f1d485085de9176822e3e7246576c7be2cc
ChangDaze <explision000@gmail.com>
2026-03-12 06:38:48 +0800

    [main][feat]1009ComplementofBase10Integer

---
+++ ProblemSolveConsole/LeetCode/1009ComplementofBase10Integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1009ComplementofBase10Integer
+    {
+        public int BitwiseComplement(int n)
+        {            
+            if(n == 0) return 1; //edge case
+
+            int temp = n;
+            int mask = 1;
+            while (temp != 0)
+            {
+                temp >>= 1;
+                mask <<= 1;
+            }
+            mask -= 1;
+
+            return mask ^ n;
+        }
+        //題目只是要找補數而已...要看清楚題目base10，沒有要再另外處理
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1009ComplementofBase10IntegerTest.cs
@@ -0,0 +1,47 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1009ComplementofBase10IntegerTest
+    {
+        private _1009ComplementofBase10Integer _1009ComplementofBase10Integer;
+
+        public _1009ComplementofBase10IntegerTest()
+        {
+            _1009ComplementofBase10Integer = new _1009ComplementofBase10Integer();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _1009ComplementofBase10Integer.BitwiseComplement(5);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _1009ComplementofBase10Integer.BitwiseComplement(7);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = _1009ComplementofBase10Integer.BitwiseComplement(10);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int result = _1009ComplementofBase10Integer.BitwiseComplement(0);
+            Assert.Equal(1, result);
+        }
+    }
+}
