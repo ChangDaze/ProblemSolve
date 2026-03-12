26f19fa5755a8211825cbffe27b1db7be5c8817b
ChangDaze <explision000@gmail.com>
2026-03-13 05:29:55 +0800

    [main][feat]0686RepeatedStringMatch

---
+++ ProblemSolveConsole/LeetCode/0686RepeatedStringMatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _0686RepeatedStringMatch
+    {
+        public int RepeatedStringMatch(string a, string b)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+
+            while(sb.Length < b.Length) //可同時判斷空字串
+            {
+                sb.Append(a);
+                count++;
+            }
+
+            if(sb.ToString().Contains(b)) return count;
+
+            sb.Append(a);//因為會有可能起始的邊界不同 ex: abcd vs cdabcdab  => abcdabcd false abcdabcdabcd true
+            //ex: 長度雖是2但是位置是0.5 ~ 2.5(起始點會是0~1 => 所以結束點會是 2 ~ 3)所以要組成3才能完全contain，數字類比index
+            count++;
+            if (sb.ToString().Contains(b)) return count;
+
+            return -1;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/0686RepeatedStringMatchTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _0686RepeatedStringMatchTest
+    {
+        private _0686RepeatedStringMatch _0686RepeatedStringMatch;
+
+        public _0686RepeatedStringMatchTest()
+        {
+            _0686RepeatedStringMatch = new _0686RepeatedStringMatch();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _0686RepeatedStringMatch.RepeatedStringMatch("abcd", "cdabcdab");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _0686RepeatedStringMatch.RepeatedStringMatch("a", "aa");
+            Assert.Equal(2, result);
+        }
+    }
+}
