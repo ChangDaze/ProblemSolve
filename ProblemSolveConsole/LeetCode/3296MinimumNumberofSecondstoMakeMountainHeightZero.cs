c47747096bbd88a25f9535decd850c35081601a0
ChangDaze <explision000@gmail.com>
2026-03-14 05:38:01 +0800

    [main][feat]3296MinimumNumberofSecondstoMakeMountainHeightZero

---
+++ ProblemSolveConsole/LeetCode/3296MinimumNumberofSecondstoMakeMountainHeightZero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _3296MinimumNumberofSecondstoMakeMountainHeightZero
+    {
+        public long MinNumberOfSeconds(int mountainHeight, int[] workerTimes)
+        {
+            PriorityQueue<long[], long> pq = new PriorityQueue<long[], long>(); //[cost, futurecount, used], needsecond
+
+            foreach (int worker in workerTimes)
+            {
+                pq.Enqueue([worker, 1, 0], worker);
+            }
+
+            while(mountainHeight > 0)
+            {
+                long[] workerInfo = pq.Dequeue();
+                mountainHeight -= 1;
+                workerInfo[2] += workerInfo[0] * workerInfo[1];
+                workerInfo[1]++;
+                pq.Enqueue(workerInfo, workerInfo[2] + workerInfo[0] * workerInfo[1]);
+            }
+
+            long result = 0;
+
+            while(pq.Count > 0)
+            {
+                long[] workerInfo = pq.Dequeue();
+                if (workerInfo[2] > result)
+                {
+                    result = workerInfo[2];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/3296MinimumNumberofSecondstoMakeMountainHeightZeroTest.cs
@@ -0,0 +1,33 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _3296MinimumNumberofSecondstoMakeMountainHeightZeroTest
+    {
+        private _3296MinimumNumberofSecondstoMakeMountainHeightZero _3296MinimumNumberofSecondstoMakeMountainHeightZero;
+
+        public _3296MinimumNumberofSecondstoMakeMountainHeightZeroTest()
+        {
+            _3296MinimumNumberofSecondstoMakeMountainHeightZero = new _3296MinimumNumberofSecondstoMakeMountainHeightZero();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            long result = _3296MinimumNumberofSecondstoMakeMountainHeightZero.MinNumberOfSeconds(4, [2, 1, 1]);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            long result = _3296MinimumNumberofSecondstoMakeMountainHeightZero.MinNumberOfSeconds(10, [3, 2, 2, 4]);
+            Assert.Equal(12, result);
+        }
+    }
+}
