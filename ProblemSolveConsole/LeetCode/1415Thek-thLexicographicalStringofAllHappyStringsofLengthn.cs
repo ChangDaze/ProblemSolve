a8b51bad2fa03c99bb77fb7fb4379bcf120a1fc7
ChangDaze <explision000@gmail.com>
2026-03-14 16:22:09 +0800

    [main][feat]1415Thek_thLexicographicalStringofAllHappyStringsofLengthn

---
+++ ProblemSolveConsole/LeetCode/1415Thek-thLexicographicalStringofAllHappyStringsofLengthn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+#if false
+    public class Solution
+    {
+        int k = 0;
+        string result = "";
+        List<char> happyChars = new List<char>() { 'a', 'b', 'c' };
+        public string GetHappyString(int n, int k)
+        {
+            this.k = k;
+            Recursive("", ' ', n);
+            return result;
+        }
+
+        private void Recursive(string str, char preChar, int n)
+        {
+            if (str.Length == n)
+            {
+                k--;
+                if (k == 0) result = str;
+                return;
+            }
+
+            foreach (char c in happyChars)
+            {
+                if (c == preChar) continue;
+                Recursive(str + c, c, n);
+            }
+        }
+    }
+#endif
+    public class _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn
+    {
+        public string GetHappyString(int n, int k)
+        {
+            char[] chars = ['a', 'b', 'c'];
+            List<char> result = new List<char>();
+            int count = 0;
+            Recursive(chars, 0, result, k, n,ref count);
+            return String.Concat(result);
+        }
+
+        private void Recursive(char[] chars, int layer, List<char> result, int k, int n, ref int count)
+        {
+            if(result.Count == n)
+            {
+                count++;
+                return;
+            }
+
+            foreach (char c in chars)
+            {
+                if(layer != 0 && result[layer - 1] == c)
+                {
+                    continue;                    
+                }
+
+                result.Add(c);
+                Recursive(chars, layer + 1, result, k, n, ref count);
+                if (count == k)
+                {
+                    break;
+                }
+                result.RemoveAt(layer);
+
+                
+            }
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1415Thek-thLexicographicalStringofAllHappyStringsofLengthnTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1415Thek_thLexicographicalStringofAllHappyStringsofLengthnTest
+    {
+        private _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn;
+
+        public _1415Thek_thLexicographicalStringofAllHappyStringsofLengthnTest()
+        {
+            _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn = new _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            string result = _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn.GetHappyString(1,3);
+            Assert.Equal("c", result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string result = _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn.GetHappyString(1, 4);
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string result = _1415Thek_thLexicographicalStringofAllHappyStringsofLengthn.GetHappyString(3, 9);
+            Assert.Equal("cab", result);
+        }
+    }
+}
