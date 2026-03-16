e2c139ce4151cac70a1788e2cd846bf4f033b030
ChangDaze <explision000@gmail.com>
2026-03-17 06:39:29 +0800

    [main][feat]1878GetBiggestThreeRhombusSumsinaGrid

---
+++ ProblemSolveConsole/LeetCode/1878GetBiggestThreeRhombusSumsinaGrid.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _1878GetBiggestThreeRhombusSumsinaGrid
+    {
+        public int[] GetBiggestThree(int[][] grid)
+        {
+            //grid可能不是正方形
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            SortedSet<int> st = new SortedSet<int>();
+
+            //k為point跟中心的距離
+            //top point (i,j) => 每個cell作為top全檢查一遍
+            //right point (i + k, j + k)
+            //left point (i + k, j - k)
+            //bottom point (i + 2k, j)
+
+            for (int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    st.Add(grid[i][j]); // k = 0 rhombus
+
+                    for(int k = 1; ; k++) //k沒限制上限，但當超出邊界就break換檢查下個cell
+                    {
+                        int down = i + 2 * k;
+                        int left = j - k;
+                        int right = j + k;
+
+                        if (down >= m || left < 0 || right >= n) break;
+
+                        int sum = 0; //rhombus總和
+
+                        int x = i, y = j;
+
+                        //top -> right (不含right)
+                        for(int t = 0; t < k; t++) 
+                        {
+                            sum += grid[x+t][y+t];
+                        }
+
+                        //right -> bottom (不含bottom)
+                        for (int t = 0; t < k; t++)
+                        {
+                            sum += grid[x + k + t][y + k - t];
+                        }
+
+                        //bottom -> left (不含left)
+                        for (int t = 0; t < k; t++)
+                        {
+                            sum += grid[x + 2 * k - t][y - t];
+                        }
+
+                        //left -> top (不含top)
+                        for (int t = 0; t < k; t++)
+                        {
+                            sum += grid[x + k - t][y - k + t];
+                        }
+
+                        st.Add(sum);
+                    }
+                }
+            }
+
+            return st.Reverse().Take(3).ToArray(); //不足3個不會報錯
+        }
+
+        //抄這位的
+        //https://leetcode.com/problems/get-biggest-three-rhombus-sums-in-a-grid/solutions/7650942/brute-force-rhombus-enumeration-border-t-8pyi/?envType=daily-question&envId=2026-03-16
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/1878GetBiggestThreeRhombusSumsinaGridTest.cs
@@ -0,0 +1,40 @@
+﻿using ProblemSolveConsole.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsoleUnitTest.LeetCode
+{
+    public class _1878GetBiggestThreeRhombusSumsinaGridTest
+    {
+        private _1878GetBiggestThreeRhombusSumsinaGrid _1878GetBiggestThreeRhombusSumsinaGrid;
+
+        public _1878GetBiggestThreeRhombusSumsinaGridTest()
+        {
+            _1878GetBiggestThreeRhombusSumsinaGrid = new _1878GetBiggestThreeRhombusSumsinaGrid();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] result = _1878GetBiggestThreeRhombusSumsinaGrid.GetBiggestThree([[3, 4, 5, 1, 3], [3, 3, 4, 2, 3], [20, 30, 200, 40, 10], [1, 5, 5, 4, 1], [4, 3, 2, 2, 5]]);
+            Assert.Equal([0, 1, 3, 2], result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] result = _1878GetBiggestThreeRhombusSumsinaGrid.GetBiggestThree([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+            Assert.Equal([20, 9, 8], result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] result = _1878GetBiggestThreeRhombusSumsinaGrid.GetBiggestThree([[7, 7, 7]]);
+            Assert.Equal([7], result);
+        }
+    }
+}
