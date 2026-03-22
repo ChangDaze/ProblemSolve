da127badf597f166c22a9b53a23ccef7ff634a68
ChangDaze <explision000@gmail.com>
2026-03-22 16:05:17 +0800

    [main][feat]3070CountSubmatriceswithTop_LeftElementandSumLessThank

---
+++ ProblemSolveConsole/LeetCode/3070CountSubmatriceswithTop-LeftElementandSumLessThank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _3070CountSubmatriceswithTop_LeftElementandSumLessThank
+    {
+        public int CountSubmatrices(int[][] grid, int k)
+        {
+            int result = 0;
+            int x = grid.Length;
+            int y = grid[0].Length;
+
+            //建立prefixSum
+            int[][] map = new int[x][];
+            for(int i = 0 ; i < x; i++)
+            {
+                map[i] = new int[y];
+            }
+
+            //loop
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    int temp = 0;
+                    if (i - 1 >= 0)
+                    {
+                        temp += map[i-1][j];
+                    }
+
+                    if(j - 1 >= 0)
+                    {
+                        temp += map[i][j-1];
+                    }
+
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                    {
+                        temp -= map[i - 1][j - 1];
+                    }
+
+                    temp += grid[i][j];
+
+                    map[i][j] = temp;
+
+                    if (temp <= k)
+                    {                        
+                        result++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemSolveConsoleUnitTest/LeetCode/3070CountSubmatriceswithTop_LeftElementandSumLessThankTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolveConsole.LeetCode
+{
+    public class _3070CountSubmatriceswithTop_LeftElementandSumLessThankTest
+    {
+        private _3070CountSubmatriceswithTop_LeftElementandSumLessThank _3070CountSubmatriceswithTop_LeftElementandSumLessThank;
+
+        public _3070CountSubmatriceswithTop_LeftElementandSumLessThankTest()
+        {
+            _3070CountSubmatriceswithTop_LeftElementandSumLessThank = new _3070CountSubmatriceswithTop_LeftElementandSumLessThank();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int result = _3070CountSubmatriceswithTop_LeftElementandSumLessThank.CountSubmatrices([[7, 6, 3], [6, 6, 1]], 18);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = _3070CountSubmatriceswithTop_LeftElementandSumLessThank.CountSubmatrices([[7, 2, 9], [1, 5, 0], [2, 6, 6]], 20);
+            Assert.Equal(6, result);
+        }
+    }
+}
