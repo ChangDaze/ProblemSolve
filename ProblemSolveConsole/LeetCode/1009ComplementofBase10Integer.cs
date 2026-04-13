4f9a7ba36725a0ab5a093846ca37fae61eb14e90
ChangDaze <explision000@gmail.com>
2026-04-14 03:40:19 +0800

    [main][feat]1009ComplementofBase10Integer註解

--- ProblemSolveConsole/LeetCode/1009ComplementofBase10Integer.cs
+++ ProblemSolveConsole/LeetCode/1009ComplementofBase10Integer.cs
@@ -9,11 +9,14 @@ namespace ProblemSolveConsole.LeetCode
     public class _1009ComplementofBase10Integer
     {
         public int BitwiseComplement(int n)
-        {            
-            if(n == 0) return 1; //edge case
+        {
+            //Time complexity:O(log(n)) => log2 往下降
+            //Space complexity:O(1)
+            if (n == 0) return 1; //edge case
 
+            //製作mask
             int temp = n;
-            int mask = 1;
+            int mask = 1; //mask 最初比 n 多一bit ex: n = 101 (bit), mask 最終等於 1000(bit) - 1 = 111(bit)
             while (temp != 0)
             {
                 temp >>= 1;
@@ -21,8 +24,14 @@ namespace ProblemSolveConsole.LeetCode
             }
             mask -= 1;
 
-            return mask ^ n;
+            return mask ^ n; //xor 就是補數，雖然bit xor 但value 還是 int 所以不用特別轉換
         }
-        //題目只是要找補數而已...要看清楚題目base10，沒有要再另外處理
+
+        //https://leetcode.com/problems/complement-of-base-10-integer/solutions/3544067/solution-by-deleted_user-b37f/
+        //這個方法也是做mask，不過跟我有點差異，他得比較直接做出1111bit mask，是個很好的參考
+        //https://leetcode.com/problems/complement-of-base-10-integer/solutions/256740/javacpython-find-1111111-n-by-lee215-vgm0/
+        //這位方法跟第一位一樣，不過沒用shift用*2
+        //上面兩位的方法可以不用特別處理if(num == 0) return 1;，值得記住
+        //大家大差不差都用bit masking，雖然approach有所差異但因為是log(n)所以核心概念和速度理論不會差太多
     }
 }
